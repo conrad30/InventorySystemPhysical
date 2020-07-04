5bffe153e8c814ccb30a7b3938b95ce6dc75a247
conrad30 <46958188+conrad30@users.noreply.github.com>
2020-07-04 17:08:58 +0800

    Update Wrapper.vue

--- components/navi/Wrapper.vue
+++ components/navi/Wrapper.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>
-        <menuLink :url="'/dashboard'">
+    <div class="flex shadow-xl bg-white w-full items-center justify-between">
+        <menuLink :url="'/dashboard'" class="text-white bg-teal-600 hover:bg-teal-700">
         <template v-slot:icon-word>
-            LEAD
+            <div class="flex items-center justify-start text-lg overflow-x-hidden">
+                <i class= "fab fa-canadian-maple-leaf ml-6 mr-4"></i>
+                <label class ="ml-2 font-medium font-sans tracking-wide"> LEAD </label>
+            </div>
         </template>
         </menuLink>
-
-        <naviButton>
+            
+        <naviButton
+                class="hover:bg-gray-300 hover:text-gray-600 mx-1">
             <template v-slot:button-icon>
                 <i class="fas fa-bars"></i>
             </template>
