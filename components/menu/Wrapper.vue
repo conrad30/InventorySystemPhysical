90648c340dda9548dd175ab38a02f23872668fbf
conrad30 <46958188+conrad30@users.noreply.github.com>
2020-07-05 11:08:20 +0800

    Tutorial 7 Phyical
    Tutorial Physical Class

---
+++ components/menu/Wrapper.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="
+        flex
+        flex-col
+        w-56
+        transition-all
+        duration-500
+        ease-in-out
+        bg-gray-800
+        h-screen
+        shadow-2xl
+        pt-16
+    ">
+    
+    <menuLink
+        :url="'/dashboard'"
+        :class="{
+            'text-gray-500':!$route.path.includes('/dashboard'),
+            'text-white':$route.path.includes('/dashboard'),
+            'hover:bg-gray-600':true,
+            'hover:text-white':true
+            }">
+            <template v-slot:icon-word>
+                <div class="flex items-center justify-start overflow-x-hidden">
+                    <i class="fas fa-home ml-6 mr-4"></i>
+                    <label class="flex-shrink-0 ml-2 text-sm font-sans tracking-wide cursor-pointer">Dashboard</label>
+                </div>
+    </template>
+    </menuLink>
+        <menuLinkList :url="'/products'"
+                  :class="{
+                  'text-gray-500':!$route.path.includes('/products'),
+                  'text-white':$route.path.includes('/products'),
+                  'cursor-pointer':true
+                  }">
+                  <template v-slot:icon-word>
+                      <div class="flex items-center justify-start overflow-x-hidden hover:text-white">
+                          <i class="fas fa-truck-loading ml-6 mr-4"></i>
+                          <label class="flex-shrink-0 ml-2 text-sm font-sans tracking-wide cursor-pointer">Products</label>
+                        </div>
+                 </template>
+                 <template v-slot:sub-link>
+                </template>
+    </menuLinkLIst>
+    
+    </div>
+</template>
+
+
+<script>
+    import MenuLink from "~/components/items/MenuLink.vue";
+    import MenuLinkList from "~/components/items/MenuLinkList.vue";
+    export default{
+        components:{
+            MenuLink,
+            MenuLinkList
+        }
+
+    }
+</script>
--- components/navi/Wrapper.vue
+++ components/navi/Wrapper.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="flex shadow-xl bg-white w-full items-center justify-between">
+        <div class="flex">
         <menuLink :url="'/dashboard'" class="text-white bg-teal-600 hover:bg-teal-700">
         <template v-slot:icon-word>
             <div class="flex items-center justify-start text-lg overflow-x-hidden">
@@ -15,7 +16,7 @@
                 <i class="fas fa-bars"></i>
             </template>
         </naviButton>
-
+        </div>
     </div>
 </template>
 <script>
