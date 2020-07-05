b70be442e1312b4d3dcdc8f3a046b64a27caa118
conrad30 <46958188+conrad30@users.noreply.github.com>
2020-07-05 11:23:23 +0800

    Update Wrapper.vue

--- components/menu/Wrapper.vue
+++ components/menu/Wrapper.vue
@@ -40,6 +40,23 @@
                         </div>
                  </template>
                  <template v-slot:sub-link>
+                     <menuLink
+                        :url="'/products/new'"
+                        :class="{
+                            'text-gray-500':!$route.path.includes('/products/new'),
+                            'text-white':$route.path.includes('/products/new'),
+                            'hover:text-white':true,
+                            'cursor-pointer':true
+                        }">
+                        <template v-slot:icon-word>
+                            <div class="flex items-center justify-start overflow-hidden py-2">
+                                <i class ="fas fa-coffee ml-6 mr-4"></i>
+                                <label class="flex-shrink-0 ml-2 text-sm
+                                font-sans tracking-wide cursor-pointer">New Product</label>
+                            </div>
+                        </template>   
+                      </menuLink> 
+                          
                 </template>
     </menuLinkLIst>
     
